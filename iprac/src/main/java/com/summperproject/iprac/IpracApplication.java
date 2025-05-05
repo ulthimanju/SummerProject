89fbeab3d33bb9d03811d541318d81df9041554d
ulthimanju <manju.ulthi@gmail.com>
2025-05-05 22:56:16 +0530

    Initial project setup

---
+++ iprac/src/main/java/com/summperproject/iprac/IpracApplication.java
@@ -0,0 +1,13 @@
+package com.summperproject.iprac;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IpracApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IpracApplication.class, args);
+	}
+
+}
---
+++ iprac/src/test/java/com/summperproject/iprac/IpracApplicationTests.java
@@ -0,0 +1,13 @@
+package com.summperproject.iprac;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IpracApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
